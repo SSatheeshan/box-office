884a32ec4c5869a4cbef6522c16e9cb7fa9a2905
Satheeshan <satheeshan.satheesh26@gmail.com>
2022-01-28 23:25:01 +0530

    added router and learned some basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="about" element={<About />} />
+      <Route>this is 404 not found page</Route>
+    </Routes>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
 }
 
 export default App;
